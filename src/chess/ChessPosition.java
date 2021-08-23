88488d228a720e93d548cc3d2264be68958a73fb
Emanuel Martins <emanuelmartins284@gmail.com>
2021-08-23 16:06:51 -0300

    fix: typo again lol

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -22,7 +22,7 @@ public class ChessPosition {
         return row;
     }
 
-    //That method below recieves a normal chess position (ex b, 6)
+    //That method below receives a normal chess position (ex b, 6)
     // and converts it into a Matrix position (ex: 2, 1) to place a piece into the board.
     protected Position toPosition(){
         return new Position(8 - row, column - 'a'); //Column - 'a' is to get de distance between the column letter informed and the initial letter a with unicode subtraction.
