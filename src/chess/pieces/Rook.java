92f9f6a5986e023da6943722fec8fca9885478dd
Emanuel Martins <emanuelmartins284@gmail.com>
2021-08-30 17:34:19 -0300

    Implementing possible moves for Rook

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -19,6 +20,61 @@ public class Rook extends ChessPiece {
     public boolean[][] possibleMoves() {
         //TODO implement specific moves for rook
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p =  new Position(0,0);
+
+        /* above - it is verifying first if the position above exists and if there isn't a piece there.
+           if the condition is true, it ll mark that position as true (free to go) */
+        p.setValues(position.getRow() - 1 , position.getColumn());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow()- 1);
+        }
+
+        /* above - if the upper condition was false, it'll go out of the while and test if that last position exists and if there is an opponent piece
+        if that condition is true it'll mark the position free to go as well */
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // left
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+
+        // right
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // below
+        p.setValues(position.getRow() + 1 , position.getColumn());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
+        }
+
+        /* above - if the upper condition was false, it'll go out of the while and test if that last position exists and if there is an opponent piece
+        if that condition is true it'll mark the position free to go as well */
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+
+
         return mat;
     }
 }
