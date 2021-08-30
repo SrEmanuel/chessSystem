766996380cb3bca60301f4c63e3eb65c368fc18f
Emanuel Martins <emanuelmartins284@gmail.com>
2021-08-30 17:01:43 -0300

    Initial implementation of possible moves of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -14,4 +14,11 @@ public class King extends ChessPiece {
     public String toString(){
         return "K";
     }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        //TODO implement specific moves for king
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
+    }
 }
